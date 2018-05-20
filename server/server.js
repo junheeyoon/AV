a5638a75b5de7c510a52ea4bdc782cddc2c03ac5
junheeyoon <enteryjh@ajou.ac.kr>
2018-05-20 18:02:34 +0900

    update

--- server/server.js
+++ server/server.js
@@ -99,30 +99,49 @@ server.post('/device/', function (req, res, next) {
     });
 });
 
-server.delete('/user/', function (req, res, next) {
-    connection.query("SELECT * FROM family", function(err, result, fields){
+server.delete('/user/:user_id', function (req, res, next) {
+    connection.query("DELETE * FROM user WHERE user_id = ?", req.params.user_id, function(err, result, fields){
         if(err){
-            console.log(err);
-            console.log("쿼리문에 오류가 있습니다.");
+            res.json({
+                result : {
+                        isOk : false, 
+                        error : "쿼리문에 오류가 있습니다."
+                }
+            });
         }
         else{
             console.log('Access');
             console.log(result);
-            res.json(result);
+            res.json({
+                result : {
+                        isOk : true, 
+                        message : req.params.user_id + '가 삭제되었습니다.'
+                }
+            });
         }
+        
     });
 }); 
 
-server.delete('/device/', function (req, res, next) {
-    connection.query("SELECT * FROM family", function(err, result, fields){
+server.delete('/device/:device_id', function (req, res, next) {
+    connection.query("DELETE * FROM object WHERE device_id = ?", req.params.device_id, function(err, result, fields){
         if(err){
-            console.log(err);
-            console.log("쿼리문에 오류가 있습니다.");
+            res.json({
+                result : {
+                        isOk : false, 
+                        error : "쿼리문에 오류가 있습니다."
+                }
+            });
         }
         else{
             console.log('Access');
             console.log(result);
-            res.json(result);
+            res.json({
+                result : {
+                        isOk : true, 
+                        message : req.params.device_id + '가 삭제되었습니다.'
+                }
+            });
         }
     });
 }); 

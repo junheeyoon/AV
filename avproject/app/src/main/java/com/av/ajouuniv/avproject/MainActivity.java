7336cd186c229df420eb53925ac17f2f3b125a6a
DESKTOP-7UP2R9N\soulm <soulmade00@gmail.com>
2018-05-24 00:35:53 +0900

    메인페이지 구현

---
+++ avproject/app/src/main/java/com/av/ajouuniv/avproject/MainActivity.java
@@ -0,0 +1,78 @@
+package com.av.ajouuniv.avproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import com.ajouuniv.linphone.service.Linphone;
+import com.ajouuniv.linphone.service.callback.PhoneCallback;
+
+import org.linphone.core.LinphoneCall;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @BindView(R.id.hang_up) Button mHangUp;
+    @BindView(R.id.accept_call) Button mCallIn;
+    @BindView(R.id.toggle_speaker) Button mToggleSpeaker;
+    @BindView(R.id.toggle_mute) Button mToggleMute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Linphone.addCallback(null, new PhoneCallback() {
+            @Override
+            public void incomingCall(LinphoneCall linphoneCall) {
+                super.incomingCall(linphoneCall);
+                Linphone.toggleSpeaker(true);
+                mCallIn.setVisibility(View.VISIBLE);
+                mHangUp.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void callConnected() {
+                super.callConnected();
+                Linphone.toggleSpeaker(Linphone.getVideoEnabled());
+                Linphone.toggleMicro(false);
+                mCallIn.setVisibility(View.GONE);
+                mToggleSpeaker.setVisibility(View.VISIBLE);
+                mToggleMute.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void callEnd() {
+                super.callEnd();
+                mCallIn.setVisibility(View.GONE);
+                mHangUp.setVisibility(View.GONE);
+                mToggleMute.setVisibility(View.GONE);
+                mToggleSpeaker.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    @OnClick(R.id.hang_up)
+    public void hangUp() {
+        Linphone.hangUp();
+    }
+
+    @OnClick(R.id.accept_call)
+    public void acceptCall() {
+        Linphone.acceptCall();
+    }
+
+    @OnClick(R.id.toggle_mute)
+    public void toggleMute() {
+        Linphone.toggleMicro(!Linphone.getLC().isMicMuted());
+    }
+
+    @OnClick(R.id.toggle_speaker)
+    public void toggleSpeaker() {
+        Linphone.toggleSpeaker(!Linphone.getLC().isSpeakerEnabled());
+    }
+}

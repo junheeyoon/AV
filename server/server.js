6e2fbfa0d77594359b0eda25257338669e1bf408
junheeyoon <enteryjh@ajou.ac.kr>
2018-05-25 00:28:32 +0900

    update

--- server/server.js
+++ server/server.js
@@ -123,7 +123,7 @@ server.post('/user/', function (req, res, next) {
 }); 
 
 server.post('/device/', function (req, res, next) {
-    res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+    //res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
     var body = req.body;
     connection.query("INSERT INTO user SET ?", body, function(err, result, fields){
         if(err){

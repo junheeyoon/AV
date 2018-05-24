2b01401cb91ae3c655045aa4c427b828ac3b06aa
junheeyoon <enteryjh@ajou.ac.kr>
2018-05-25 00:28:48 +0900

    update

--- server/server.js
+++ server/server.js
@@ -150,7 +150,7 @@ server.post('/device/', function (req, res, next) {
 });
 
 server.delete('/user/:user_id', function (req, res, next) {
-    res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+    //res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
     connection.query("DELETE * FROM user WHERE user_id = ?", req.params.user_id, function(err, result, fields){
         if(err){
             console.log(err);

7e58ffe1bd7780c8505919b5e317b0d1cd2f1007
DESKTOP-10V1T1J\a <soulmade00@gmail.com>
2018-05-24 19:38:02 +0900

    api 클라이언트 추가

--- avproject2/app/src/main/java/com/av/ajouuniv/avproject2/ApiClient.java
+++ avproject2/app/src/main/java/com/av/ajouuniv/avproject2/ApiClient.java
@@ -1,5 +1,8 @@
 package com.av.ajouuniv.avproject2;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient
 {
     public static final String BASE_URL = "http://testapi.droidmentor.com/";

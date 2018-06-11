f7cc221212345447f0d0795865e06a8224956e73
junheeyoon <enteryjh@ajou.ac.kr>
2018-06-11 21:07:32 +0900

    update

--- server/server.js
+++ server/server.js
@@ -153,7 +153,7 @@ server.post('/device/', function (req, res, next) {
     console.log(sum)
     post_id = 3 - sum;
     console.log('p_id:' + post_id);
-    var post_body = "('"+post_id+"','"+0+"','"+body.message+"');"
+    var post_body = (post_id, 0, body.message);
     connection.query("INSERT INTO object(object_id, object_state, object_name) VALUES ?", post_body, function(err, result, fields){
         if(err){
             console.log(err);
